ede31297542e19072d5fcd5fbbebe02eff641d7f
gabriel nogueira <gabrielnogueira8808@gmail.com>
2024-02-28 17:18:01 -0300

    adicionando exercicio 10

---
+++ exercicios/desafioex006.10.py
@@ -0,0 +1,5 @@
+celcius = float(input("digite a temperatura atual: "))
+
+fahrenheit  =  ((celcius * 9)/5) + 32 
+
+print(f"a temperatura de Celcius {celcius} convertida ficaria em torno de {fahrenheit}") 
--- exercicios/desafioex006.8.py
+++ exercicios/desafioex006.8.py
@@ -1,8 +1,19 @@
 
+# largura = float(input("digite a largura da parede: "))
+# altura = float(input("digite a altura da parede: "))
+
+# area = altura * largura 
+# tinta = area / 2
+
+# print(f"A area da parede é de {area} e a quantidade de tinta necessaria seria de {float(tinta)} litros.")
+
+
+
 largura = float(input("digite a largura da parede: "))
 altura = float(input("digite a altura da parede: "))
 
-area = altura * largura 
+area = largura * altura  
+
 tinta = area / 2
 
 print(f"A area da parede é de {area} e a quantidade de tinta necessaria seria de {float(tinta)} litros.")
